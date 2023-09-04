b8169ac3f1378b6416bb5a1dc9667db07c4779c1
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-09-04 08:06:57 +0100

    added models

---
+++ models/Task.model.js
@@ -0,0 +1,29 @@
+const { Schema, model, isObjectIdOrHexString } = require("mongoose");
+
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title required"],
+  },
+  description: String,
+  state: {
+    type: String,
+    enum: ["Todo", "In Progress", "Done"],
+    default: "Todo",
+  },
+  responsible: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+  },
+  category: {
+    type: String,
+    enum: ["Bug", "Improvement", "Feature"],
+  },
+});
+const Task = model("Task", taskSchema);
+
+module.exports = Task;
